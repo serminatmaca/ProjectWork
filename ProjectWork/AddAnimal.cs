04b9ae3c6823dcf4b65e29d4ee7183b6d7cb0d4c
Şermin Atmaca <117165693+serminatmaca@users.noreply.github.com>
2022-12-28 22:59:29 +0300

    ProjectWork

---
+++ ProjectWork/AddAnimal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ProjectWork
+{
+	public class AddAnimal
+	{
+		public AddAnimal()
+		{
+
+		}
+
+        public string Genus { get; set; }
+
+        public string Name { get; set; }
+
+		public string Habitat { get; set; }
+
+		public int Age { get; set; }
+
+		public int Lenght { get; set;}
+
+		public int Weight { get; set; }
+    }
+}
+
---
+++ ProjectWork/Program.cs
@@ -0,0 +1,45 @@
+﻿using ProjectWork;
+
+internal class Program
+{
+    private static void Main(string[] args)
+
+    {
+        AddAnimal animal = new AddAnimal();
+
+
+        animal.Genus = "Sea Creature";
+        animal.Name = "Whale";
+        animal.Habitat = "Sea";
+        animal.Age = 46;
+        animal.Lenght = 5;
+        animal.Weight = 123;
+
+        Console.WriteLine("Balinanın cinsi : " + animal.Genus + "\n" + "Balinanın adı : " + animal.Name + "\n" + "Balinanın yaşam alanı : " + animal.Habitat + "\n" + " Balinanın yaşı : " + animal.Age + "\n" + "Balinanın boyu : " + animal.Lenght + "\n" + "Balinanın ağırlığı : " + animal.Weight + "\n");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
